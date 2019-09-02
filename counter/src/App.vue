9f34156804489f40cf5fd18c6e7b2f2b1d2e7b4f
xsu <xsu@thoughtworks.com>
2019-09-02 21:55:47 +0800

    array multi counter

--- counter/src/App.vue
+++ counter/src/App.vue
@@ -1,24 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="numInput" v-on:keyup="updateNum" />
+    <Counter
+      v-for="index in getNum()"
+      v-bind:key="index"
+      v-bind:index="index"
+      v-bind:value="getCountByIndex(index - 1)"
+      v-on:updateCounter="updateCounter"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Counter
+  },
+  data: function() {
+    return {
+      numInput: "0",
+      counts: []
+    };
+  },
+  methods: {
+    updateCounter: function(value) {
+      let oldCounts = this.counts;
+      oldCounts[value.index] = value.value;
+      this.counts = [];
+      for (let i = 0; i < oldCounts.length; i++) {
+        this.counts.push(oldCounts[i]);
+      }
+      console.log(this.counts);
+    },
+    getCountByIndex: function(index) {
+      return this.counts[index];
+    },
+    updateNum: function() {
+      let num = this.getNum();
+      while (this.counts.length < num) {
+        this.counts.push(this.counts.length + 1);
+      }
+      while (this.counts.length > num) {
+        this.counts.pop();
+      }
+    },
+    getNum: function() {
+      let isNum = function(numString) {
+        return !isNaN(parseFloat(numString)) && isFinite(numString);
+      };
+
+      let num = parseInt(this.numInput);
+      if (isNum(this.numInput) && num >= 0) {
+        return num;
+      } else {
+        return 0;
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span>{{index}}</span>
+    <button v-on:click="plus">+</button>
+    <span>{{value}}</span>
+    <button v-on:click="reverse">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: {
+    index: Number,
+    value: Number
+  },
+  methods: {
+    plus: function() {
+      this.$emit("updateCounter", { index: this.index - 1, value: this.value + 1 });
+    },
+    reverse: function() {
+      this.$emit("updateCounter", { index: this.index - 1, value: this.value - 1 });
+    }
+  }
+};
+</script>
